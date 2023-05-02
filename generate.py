9eedae56b877731dceaa24c6ea61ec2d68d330f9
Dusty <dustygalindo@gmail.com>
2023-05-02 11:27:10 -0700

    added color to sphere based on normal vectors)

--- generate.py
+++ generate.py
@@ -1,10 +1,14 @@
 from vec3_class import Color, Point3, Vec3
 from ray_class import Ray
 from tqdm import tqdm
+from math import sqrt
 
 def ray_color(ray: Ray):
-    if (hit_sphere(Point3(0,0,-1), 0.5, ray)):
-        return Color(1,0,0)
+    t = hit_sphere(Point3(0,0,-1),0.5,ray)
+    if(t > 0):
+        normal: Vec3 = (ray.at(t) - Vec3(0,0,-1))
+        normal = normal.unit_vector()
+        return 0.5 * Color(normal[0]+1,  normal[1]+1, normal[2]+1)
     unit_direction = ray.direction.unit_vector()
     t = 0.5*(unit_direction[1] + 1.0)
     return (1.0 - t)*Color(1.0, 1.0, 1.0) + t*Color(0.5,0.7,1)
@@ -15,7 +19,11 @@ def hit_sphere(center: Point3, radius, r: Ray):
     b = 2.0 * oc.dot(r.direction)
     c = oc.dot(oc) - radius * radius
     discriminant = b * b - 4 * a * c
-    return discriminant > 0
+    if(discriminant < 0):
+        return -1.0
+    else:
+        return (-b - sqrt(discriminant))/(2.0 * a)
+
 
 
 aspect_ratio = 16.0 / 9.0
