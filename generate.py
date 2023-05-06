9ef5942835d2fd6c456af2f1341090c2beb4f42f
Dusty <dustygalindo@gmail.com>
2023-05-06 01:10:21 -0700

    minor

--- generate.py
+++ generate.py
@@ -40,10 +40,8 @@ world.add(Sphere(Point3(0,-100.5,-1), 100, material_ground))
 world.add(Sphere(Point3(0, 0, -1), 0.5, material_center))
 world.add(Sphere(Point3(-1, 0, -1), 0.5, material_left))
 world.add(Sphere(Point3(1, 0, -1), 0.5, material_right))
+# world.add(Torus(Point3(-1, 0, -1), 0.3, 0.2, material_left))
 
-# world.add(Sphere(Point3(0,0,-1), 0.5))
-# world.add(Sphere(Point3(0,-100.5,-1), 100))
-# world.add(Torus(Point3(0,0,-1.5), 0.5, 0.2))
 
 # image
 aspect_ratio = 16.0 / 9.0
